97f80629004770c37063199ec8e2d8f93369fda2
tpeigne <thomas.peigne@gmail.com>
2013-05-24 20:42:16 +0200

    update modules translation structure

--- modules/responsivetopbar/translations/fr.php
+++ modules/responsivetopbar/translations/fr.php
@@ -33,3 +33,15 @@ $_MODULE['<{responsivetopbar}prestashop>responsivetopbar_9e65b51e82f2a9b9f72ebe3
 $_MODULE['<{responsivetopbar}prestashop>responsivetopbar_ba794350deb07c0c96fe73bd12239059'] = 'Emballage';
 $_MODULE['<{responsivetopbar}prestashop>responsivetopbar_377e99e7404b414341a9621f7fb3f906'] = 'Commander';
 $_MODULE['<{responsivetopbar}prestashop>responsivetopbar_0c3bf3014aafb90201805e45b5e62881'] = 'Voir mon panier';
+$_MODULE['<{responsivetopbar}prestashop>responsivetopbar_a51a9d164f0ded9abd07025c620bf9e2'] = 'Responsive top bar';
+$_MODULE['<{responsivetopbar}prestashop>responsivetopbar_e227615234794355a6009819491515d3'] = 'Ajoute une top barre menu en haut de votre site';
+$_MODULE['<{responsivetopbar}prestashop>responsivetopbar_83218ac34c1834c26781fe4bde918ee4'] = 'Bienvenue';
+$_MODULE['<{responsivetopbar}prestashop>responsivetopbar_4b877ba8588b19f1b278510bf2b57ebb'] = 'Me déconnecter';
+$_MODULE['<{responsivetopbar}prestashop>responsivetopbar_4394c8d8e63c470de62ced3ae85de5ae'] = 'Déconnexion';
+$_MODULE['<{responsivetopbar}prestashop>responsivetopbar_bffe9a3c9a7e00ba00a11749e022d911'] = 'identifiez-vous';
+$_MODULE['<{responsivetopbar}prestashop>responsivetopbar_a0623b78a5f2cfe415d9dbbd4428ea40'] = 'Votre compte';
+$_MODULE['<{responsivetopbar}prestashop>responsivetopbar_12641546686fe11aaeb3b3c43a18c1b3'] = 'Votre panier d\'achat';
+$_MODULE['<{responsivetopbar}prestashop>responsivetopbar_7fc68677a16caa0f02826182468617e6'] = 'Panier :';
+$_MODULE['<{responsivetopbar}prestashop>responsivetopbar_f5bf48aa40cad7891eb709fcf1fde128'] = 'produit';
+$_MODULE['<{responsivetopbar}prestashop>responsivetopbar_86024cad1e83101d97359d7351051156'] = 'produits';
+$_MODULE['<{responsivetopbar}prestashop>responsivetopbar_9e65b51e82f2a9b9f72ebe3e083582bb'] = '(vide)';
