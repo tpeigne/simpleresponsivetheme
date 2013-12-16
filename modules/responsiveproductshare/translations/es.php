52c3cc4fbe6a83053ddb288b7f946c2a934f5394
Antonio Gámez <ellocoyo19@yahoo.es>
2013-12-16 19:59:06 +0100

    Added Spanish module translation

---
+++ modules/responsiveproductshare/translations/es.php
@@ -0,0 +1,6 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{responsiveproductshare}prestashop>responsiveproductshare_13eac0b6526cf812f4c7d6df498d9ac9'] = 'Compartir producto responsive';
+$_MODULE['<{responsiveproductshare}prestashop>responsiveproductshare_07af17c8892a87686e6415428c15a7d2'] = 'Añade un bloque responsive para compartir en la página del producto.';
